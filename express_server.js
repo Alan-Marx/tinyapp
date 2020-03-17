6e13a42361aac11d0d0c7f57b8dc9ef2f6b79b95
Alan Marx <alanmarx@hotmail.com>
2020-03-17 16:48:43 +0000

    added and changed some comments

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const app = express(); // app is now a server
-const PORT = 8080; // default port 8080
-const bodyParser = require('body-parser');
+const app = express(); 
+const PORT = 8080; 
+const bodyParser = require('body-parser'); 
 
-app.use(bodyParser.urlencoded({extended: true})); // The body-parser library will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the key body.
+// The body-parser library will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the 'body' key 
+app.use(bodyParser.urlencoded({extended: true})); 
 
-app.set('view engine', 'ejs'); // sets the ejs dependency as the templating engine
+// sets the ejs dependency as the templating engine
+app.set('view engine', 'ejs');
 
 // generate a random number, convert it to an alphanumeric string using base 36. End result is a random alphanumeric string of 6 characters
 function generateRandomString() {
@@ -19,20 +21,22 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// middleware routing functions
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+// when sending variables to an esj template, we need to send them in an object format so the template can access the data via the object key
+// esj automatically looks inside the 'view' directory for any files with the .esj extension. Here we are passing the templateVars object to the urls_index template
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase }; // when sending variables to an esj template, we need to send them in an object format so the template can access the data via the object key
-  res.render('urls_index', templateVars); // esj automatically looks inside the 'view' directory for any files with the .esj extension. Here we are passing the templateVars object to the urls_index template
+  let templateVars = { urls: urlDatabase }; 
+  res.render('urls_index', templateVars); 
 });
 
 app.post('/urls', (req, res) => {
   newShortURL = generateRandomString();
   urlDatabase[newShortURL] = req.body.longURL;
   res.redirect(`/urls/${newShortURL}`);
-  res.send('Ok');
 });
 
 app.get('/urls/new', (req, res) => {
@@ -56,6 +60,7 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+// the listen method activates the app express server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
