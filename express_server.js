e16a37d72f9be21d6c08be7123c21bad1ca09cad
Alan Marx <alanmarx@hotmail.com>
2020-03-17 05:21:05 +0000

    added body parser to my server app in order to parse data from POST requests

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express(); // app is now a server
 const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true})); // The body-parser library will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the key body.
 
 app.set('view engine', 'ejs'); // sets the ejs dependency as the templating engine
 
@@ -16,6 +19,10 @@ app.get('/', (req, res) => {
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase }; // when sending variables to an esj template, we need to send them in an object format so the template can access the data via the object key
   res.render('urls_index', templateVars); // esj automatically looks inside the 'view' directory for any files with the .esj extension. Here we are passing the templateVars object to the urls_index template
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
