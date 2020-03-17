c5e9460d944328662a519ae62112940bc633f48a
Alan Marx <alanmarx@hotmail.com>
2020-03-17 22:38:27 +0000

    added a post request handler for /urls/:id that updates a long URL in the database

--- express_server.js
+++ express_server.js
@@ -48,6 +48,11 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls/:shortURL', (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.updatedURL;
+  res.redirect('/urls');
+});
+
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
