ff23b5fa85ad824a2425b295e17719ef44b9ad34
Alan Marx <alanmarx@hotmail.com>
2020-03-18 05:27:14 +0000

    changed the header.ejs and existing route handlers so that the username will be displayed on all pages if someone is logged in

--- express_server.js
+++ express_server.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 // when sending variables to an esj template, we need to send them in an object format so the template can access the data via the object key
 // esj automatically looks inside the 'view' directory for any files with the .esj extension. Here we are passing the templateVars object to the urls_index template
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase }; 
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] }; 
   res.render('urls_index', templateVars); 
 });
 
@@ -49,11 +49,12 @@ app.post('/urls', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  let templateVars = { username: req.cookies["username"] };
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render('urls_show', templateVars);
 });
 
