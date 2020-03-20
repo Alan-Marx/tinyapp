41f833602bbab6fb70e1b8972ca99984e2eac9c2
Alan Marx <alanmarx@hotmail.com>
2020-03-20 05:37:00 +0000

    put all helper functions into the helper_functions file and imported the functions into the server file

--- express_server.js
+++ express_server.js
@@ -3,33 +3,31 @@ const app = express();
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
+const { emailChecker, generateRandomString, urlsForUser, longURLChecker } = require('./helper_functions');
 const PORT = 8080;
 
-const { emailChecker } = require('./helper_functions');
-
-// The body-parser library will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the 'body' key
+// The body-parser library will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the 'body' key.
 app.use(bodyParser.urlencoded({extended: true}));
 
-// this middleware will attach the property 'session' to 'req'.
+// this middleware will allow us to encrypt the authentication cookies we send users. 
 app.use(cookieSession({
   name: 'session',
   keys: ['7ae19decbaaf9434ebc0']
 }));
 
-// sets the ejs dependency as the templating engine when sending variables to an esj template, we need to send them in an object format so the template can access the data via the object key
-// esj automatically looks inside the 'view' directory for any files with the .esj extension. Here we are passing the templateVars object to the urls_index template
+// sets the ejs dependency as the templating engine. When sending variables to an esj template, we need to send them in an object format so the template can access the data via the object key.
+// esj automatically looks inside the 'view' directory for any files with the .esj extension. 
 app.set('view engine', 'ejs');
 
-//////////////////// Databases & Classes ////////////////////
-
-// Each new long url will have a short url/key assigned to it and will have an object inside the urlDatabase corresponding to this key, containing the associated long url and the user id of the user who created it
+// Each new long url entered into our app will result in a new url object being added to this database. The short url created for the url will serve as the object key.
+// Each url object will contain a userID property, which will be sent to the user as part of an encrypted cookie for purposes of authentication.
 const urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "tqc0o4" },
-  "9sm5xK": { longURL: "http://www.google.com", userID: "tqc0o4" }
+  // Example: "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "tqc0o4" },
 };
+
 const userDatabase = {
 };
-// the userDatabase will be filled up by objects of the following class. The keys for each user object will be the same value as their id property
+// the userDatabase will be filled up by uer objects of the following class. The key of each user object will be the same as the value of their id property.
 class User {
   constructor(userRandomId, userEmail, hashedPassword) {
     this.id = userRandomId,
@@ -38,43 +36,17 @@ class User {
   }
 }
 
-///////////////////////// Functions /////////////////////
-
-// generate a random number, convert it to an alphanumeric string using base 36. End result is a random alphanumeric string of 6 characters
-const generateRandomString = () => {
-  let randomArr = Math.random().toString(36).slice(2).split('');
-  randomArr.length = 6;
-  return randomArr.join('');
-};
-
-// this function takes in a user id and returns an object containing all the url objects with a matching user id.
-const urlsForUser = (id) => {
-  const userUrls = {};
-  for (let url in urlDatabase) {
-    if (urlDatabase[url].userID === id) {
-      userUrls[url] = urlDatabase[url];
-    }
-  }
-  return userUrls;
-};
-// this function returns true if a long url has already been used by a user before
-const longURLChecker = (userUrls, longUrl) => {
-  for (let url in userUrls) {
-    if (userUrls[url].longURL === longUrl) {
-      return true;
-    }
-  }
-  return false;
-};
-
-//////////////////////// Route Handlers /////////////////////
-
 // The user variable, which is either a user object or undefined, is passed on to the template so that the headers partial can know how to display the navigation bar
 app.get('/register', (req, res) => {
+  if (req.session.user_id) {
+    res.redirect('/urls');
+    return;
+  } else {
   let user = userDatabase[req.session.user_id];
   let templateVars = { user };
   res.render('urls_register', templateVars);
   return;
+  }
 });
 
 // as long as an email and password are provided and the email has not already been registered, a new user object is added to the user database and the client is given a user id cookie
@@ -90,7 +62,6 @@ app.post('/register', (req, res) => {
     let hashedPassword = bcrypt.hashSync(req.body.password, 10);
     userDatabase[userRandomId] = new User(userRandomId,req.body.email, hashedPassword);
     req.session.user_id = userRandomId;
-    //res.cookie('user_id', userRandomId);
     res.redirect('/urls');
     return;
   }
@@ -101,7 +72,7 @@ app.post('/register', (req, res) => {
 app.get('/urls', (req, res) => {
   let user = userDatabase[req.session.user_id];
   if (user) {
-    const userUrls = urlsForUser(user.id);
+    const userUrls = urlsForUser(user.id, urlDatabase);
     let templateVars = { urls: userUrls, user };
     res.render('urls_index', templateVars);
     return;
@@ -114,7 +85,7 @@ app.get('/urls', (req, res) => {
 
 // a user can create a new url object (and accompanying short url for the inputted long url), as long as they have not already used that long url before
 app.post('/urls', (req, res) => {
-  const userUrls = urlsForUser(req.session.user_id);
+  const userUrls = urlsForUser(req.session.user_id, urlDatabase);
   if (!longURLChecker(userUrls, req.body.longURL)) {
     let newShortURL = generateRandomString();
     urlDatabase[newShortURL] = { longURL: req.body.longURL, userID: req.session.user_id };
@@ -141,10 +112,15 @@ app.get('/urls/new', (req, res) => {
 
 // this route simply sends sends the template user information so that the header partial can know what to display
 app.get('/login', (req, res) => {
-  let user = userDatabase[req.session.user_id];
-  let templateVars = { user };
-  res.render('urls_login', templateVars);
-  return;
+  if (req.session.user_id) {
+    res.redirect('/urls');
+    return;
+  } else {
+    let user = userDatabase[req.session.user_id];
+    let templateVars = { user };
+    res.render('urls_login', templateVars);
+    return;
+  }
 });
 
 //if the email passed into the login page corresponds to an email of a user object in the user database, and the password for that account corresponds to the password entered into the login page, then the user is given a cookie with the user id value of their user account
--- helper_functions.js
+++ helper_functions.js
@@ -1,4 +1,4 @@
-// this function returns true if the email provided to it as an argument corresponds to an email in one of the user objects inside the user database, meaning that a user already exists for the email provided
+// this function returns true if the email provided to it as an argument corresponds to an email in one of the user objects inside the user database, meaning that a user already exists for the email provided.
 const emailChecker = (em, database) => {
   for (let id in database) {
     if (database[id].email === em) {
@@ -8,4 +8,37 @@ const emailChecker = (em, database) => {
   return false;
 };
 
-module.exports = { emailChecker };
+// generates a random number, converts it to an alphanumeric string using base 36. End result is a random alphanumeric string of 6 characters to be used as a userID.
+const generateRandomString = () => {
+  let randomArr = Math.random().toString(36).slice(2).split('');
+  randomArr.length = 6;
+  return randomArr.join('');
+};
+
+// this function takes in a user id and url database and returns an object containing all the url objects with a matching user id.
+const urlsForUser = (id, database) => {
+  const userUrls = {};
+  for (let url in database) {
+    if (database[url].userID === id) {
+      userUrls[url] = database[url];
+    }
+  }
+  return userUrls;
+};
+
+// this function returns true if a long url has already been used by a user before.
+const longURLChecker = (userUrls, longUrl) => {
+  for (let url in userUrls) {
+    if (userUrls[url].longURL === longUrl) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = { 
+  emailChecker, 
+  generateRandomString, 
+  urlsForUser,
+  longURLChecker
+};
